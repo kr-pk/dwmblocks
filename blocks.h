2eeec506358ffef7bb51331adfd3757de26c64a6
Kr. PK <pradeepkumarjha888@gmail.com>
2023-05-08 12:43:18 +0530

    my dwmblocks build

---
+++ blocks.h
@@ -0,0 +1,16 @@
+#define CMDLENGTH 100
+#define DELIMITER " | "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("music", 0, 11),
+	BLOCK("nettraf", 1, 16),
+/*	BLOCK("battery", 5, 3),*/
+	BLOCK("memory",	10, 14),
+	BLOCK("cpu", 10, 18),
+	BLOCK("volume", 0, 10),
+/*	BLOCK("bright", 6, 5),*/
+	BLOCK("internet", 5, 4),
+	BLOCK("clock", 1, 1),
+};
+
